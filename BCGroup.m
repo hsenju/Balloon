ffe5cea59a0eac9ded266dbd0da395355f12aa31
swerty <slw247@cornell.edu>
2014-06-04 19:02:40 -0400

    create group progress

---
+++ BCGroup.m
@@ -0,0 +1,13 @@
+//
+//  BCGroup.m
+//  BalloonClean
+//
+//  Created by Sean Wertheim on 6/4/14.
+//  Copyright (c) 2014 Sean Wertheim. All rights reserved.
+//
+
+#import "BCGroup.h"
+
+@implementation BCGroup
+
+@end
---
+++ GroupsTableViewController.m
@@ -0,0 +1,127 @@
+//
+//  GroupsTableViewController.m
+//  BalloonClean
+//
+//  Created by Sean Wertheim on 6/4/14.
+//  Copyright (c) 2014 Sean Wertheim. All rights reserved.
+//
+
+#import "GroupsTableViewController.h"
+#import "BCGroupCell.h"
+#import "BCGroup.h"
+
+@interface GroupsTableViewController ()
+
+@end
+
+@implementation GroupsTableViewController
+
+static const NSString *kCreateGroupsSegueIdentifier = @"createGroupSegue";
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.userGroupsArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    BCGroupCell *cell = (BCGroupCell*)[tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    if (cell == nil) {
+        cell = [[BCGroupCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    BCGroup *currentGroup = (BCGroup*)self.userGroupsArray[indexPath.row];
+    
+    cell.groupNameLabel.text = currentGroup.groupName;
+    cell.numberOfMembersLabel.text = [NSString stringWithFormat:@"%d", currentGroup.numberOfMembers];
+    cell.groupPictureImageView = [[UIImageView alloc] initWithImage:currentGroup.groupImage];
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
